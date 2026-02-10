f60161be273e844f328a290154d9b15286648fdb
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-02-10 10:33:48 +0100

    modifica correzione

--- main1.js
+++ main1.js
@@ -1,7 +1,15 @@
-let num = [10, 50, 37, 85, 74, 93, 24, 25, 79, 16];
-let sorted = num.sort((a, b) => a -b);
-console.log(sorted);
-
-
+// SELFWORK DA REVISIONARE
 // console.log(num.sort().reverse());
 //  console.log(num.sort());
+
+
+// SELFWORK REVISIONATO
+let num = [10, 50, 37, 85, 74, 93, 24, 25, 79, 16];
+let crescente = num.sort((a, b)=> a - b);
+console.log(crescente);
+let decrescente = crescente.reverse ();
+console.log(decrescente);
+
+
+
+
